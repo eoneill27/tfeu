e360327b4c24b73aedaa3b0a1fe51e28bc402f2d
Emily <oneill.emily@gmail.com>
2023-08-09 15:31:22 -0700

    Include ACF form data on home page - build structure of site

--- wp-content/themes/tfeu/templates/pages/home.php
+++ wp-content/themes/tfeu/templates/pages/home.php
@@ -7,30 +7,91 @@
 <section class="home-content">
 	<inner-column>
 		<?php
+			
 
-			if(have_rows('home_content')) {
-				while(have_rows('home_content')) {
+			$eventName = get_field('event_name');
+			$eventDate = get_field('event_date');
+			$eventTime = get_field('event_time');
+			$eventLocation = get_field('event_location');
+			$eventDescription = get_field('event_description');
+
+			if($eventName) {
+		?>
+
+		<div id="home-top">
+			<div class="long-card">
+				<h2 class="heyyou-voice">Next event - <?=$eventName?></h2>
+				<h3 class="info-voice"><?=$eventDate?></h3>
+				<h3 class="info-voice"><?=$eventTime?></h3>
+				<h3 class="info-voice"><?=$eventLocation?></h3>
+				<p class="reading-voice"><?=$eventDescription?></p>
+
+			</div>
+
+			<div class="long-card">
+				<h2 class="heyyou-voice">Current job postings</h2>
+				<ul>
+		
+<?php }
+			if(have_rows('job_info')) {
+				while(have_rows('job_info')) {
 					the_row();
 
-					if(get_row_layout() == 'home_heading') { ?>
+					$department = get_sub_field('department');
+					$jobTitle = get_sub_field('job_title');
+					$termInfo = get_sub_field('term_info');
+					$payGrade = get_sub_field('pay_grade');
+					$postURL = get_sub_field('posting_link');
+					?>
 
-						<h1 class="heyyou-voice"><?=the_sub_field('heading');?></h1>
-
+					<li class="info-voice">
+						<a href="<?=$postURL?>" target="_blank">
+							<?=$department?> - <span class="strong info-voice"><?=$jobTitle?></span> - <?=$termInfo?>, pay grade <?=$payGrade?>
+						</a>
+					</li>
+					
 					<?php } ?>
+				</ul>
+			</div>
+		</div>
+		<div id="home-bottom">
+			<?php 
+			if(have_rows('home_page_tile')) {
+				while(have_rows('home_page_tile')) {
+					the_row();
 
+					$tileHeading = get_sub_field('tile_heading');
+					$tileLinks = get_sub_field('tile_links');
+			?>
+
+				<div class="home-tile">
+					<h2 class="heyyou-voice"><?=$tileHeading?></h2>
+					<ul>
+						<?php
+						if(have_rows('tile_links')) {
+							while(have_rows('tile_links')) {
+								the_row();
+								$linkName = get_sub_field('link_name');
+								$linkURL = get_sub_field('link_url');
+						?>
+						<li class="info-voice">
+							<a href="<?=$linkURL?>"><?=$linkName?></a>
+						</li>
+					<?php } } ?>
+					</ul>
+				</div>
 					<?php 
-					if(get_row_layout() == 'home_subheading') {?>
-						<h2 class="info-voice"><?=the_sub_field('subheading');?></h2>
-					<?php } ?>
+					}
 
-					<?php
-					if(get_row_layout() == 'paragraph') { ?>
-						<p class="reading-voice"><?=the_sub_field('paragraph');?></p>
-					<?php } ?>
-				<?php } ?>
-			<?php } ?>
+				}
+			}
+					
+
+			 ?>
+
+			
+		</div>
 	</inner-column>
 </section>
 
 
-<h2>test</h2>
